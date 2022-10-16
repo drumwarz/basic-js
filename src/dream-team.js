d9099b2f44c7171ed9db767fde17cd4dd428a9ec
Ihar Sapeha <maddavoice@gmail.com>
2022-10-16 22:21:28 +0300

    feat: add cats, team, carbon

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,10 +13,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members) || members === null) {
+    return false;
+  } else {
+    return members
+      .filter((element) => typeof element === 'string')
+      .map((element) => element.trim()[0].toUpperCase())
+      .sort()
+      .join('');
+  }
+
 }
+
+
+
 
 module.exports = {
   createDreamTeam
